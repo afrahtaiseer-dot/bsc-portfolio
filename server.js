c8a4256516ddf43c4290503f54aa7155da585753
afrahtaiseer-dot <afrahtaiseer@gmail.com>
2026-03-18 14:10:47 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(__dirname));
+
+// ✅ separate DB
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Almz_0910",
+  database: "afrah_portfolio"
+});
+
+db.connect(err => {
+  if (err) console.log(err);
+  else console.log("MySQL Connected");
+});
+
+app.post("/contact", (req, res) => {
+  const { name, email, message } = req.body;
+
+  const sql = "INSERT INTO contacts (name, email, message) VALUES (?, ?, ?)";
+
+  db.query(sql, [name, email, message], (err) => {
+    if (err) return res.status(500).send("Error");
+    res.send("Saved");
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
